4659907c9513cc9da2f5d81a807690cbd9e8c224
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-27 18:05:48 +0000

    Imported MooRainbow

--- config.rb
+++ config.rb
@@ -1,15 +1,10 @@
 # encoding: utf-8
 
 # dependencies of the pupu
+dependency :mootools
 
 # load javascripts
 javascript "moorainbow"
 
 # load stylesheets
 stylesheet "moorainbow"
-
-# parameters of plugin
-# it will be blank in most cases
-#parameter :request, optional: ["local", "ajax"] do |type|
-#  javascript "lib/autocompleter.#{type}"
-#end
--- initializers/moorainbow.js
+++ initializers/moorainbow.js
@@ -1,3 +1,21 @@
+// http://moorainbow.woolly-sheep.net/#doc
 window.addEvent("domready", function() {
-  // add your code here
-})
+  // simple
+  $$(".rainbow").each(function(element) {
+    new MooRainbow("demo1", {id: "myDemo1"});
+  });
+
+  // advanced
+  //new MooRainbow("demo2", {
+  //  id: "myDemo2",
+  //  wheel: true,
+  //  onChange: function(color) {
+  //    $("outer").setStyle("background-color", color.hex);
+  //    $("demoInput2").value = color.hex;
+  //  },
+  //  onComplete: function(color) {
+  //    $("outer").setStyle("background-color", "#121212");
+  //    $("demoInput2").value = color.hex;
+  //  }
+  //});
+});

89c399aef4255b953947221c34dd979810d9ca89
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-27 17:59:45 +0000

    Initial import

---
+++ config.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+# dependencies of the pupu
+
+# load javascripts
+javascript "moorainbow"
+
+# load stylesheets
+stylesheet "moorainbow"
+
+# parameters of plugin
+# it will be blank in most cases
+#parameter :request, optional: ["local", "ajax"] do |type|
+#  javascript "lib/autocompleter.#{type}"
+#end
